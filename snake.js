f79b30c495fe4a345afdc0f693057be11ef99270
Daniel Sun <dansun@mit.edu>
2020-12-27 22:37:43 -0800

    reverse y axis render

--- snake.js
+++ snake.js
@@ -10,14 +10,14 @@ updateSnake = () => {
         snakeBody[i + 1] = { x: snakeBody[i].x, y: snakeBody[i].y }
     }
 
-    snakeBody[0].y += 0
-    snakeBody[0].x += 1
+    snakeBody[0].y += 1
+    snakeBody[0].x += 0
 }
 
 drawSnake = (gameBoard) => {
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = -segment.y
+        snakeElement.style.gridRowStart = segment.y
         snakeElement.style.gridColumnStart = segment.x
         snakeElement.classList.add('snake')
         gameBoard.appendChild(snakeElement)
